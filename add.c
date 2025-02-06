bde3d90656a2d5aaa9bd380bb06f104c13c4a020
ragul <ragulnatarajan11@gmail.com>
2025-02-06 16:50:24 +0530

    cnf in same file

--- add.c
+++ add.c
@@ -1,9 +1,9 @@
 #include"header.h"
-
+//checking for conflict in same file different fuile
 //this header by ragul
 //This header by sandy
 
 double add(double a, double b)
 {
     return a + b;
-}`
+}
