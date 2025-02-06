d279c93e6b630a7a9762cdbf8e86f415de5e852e
S Sandeep Kumar <ssandeepkumarofficial63@gmail.com>
2025-02-06 15:46:16 +0530

    header added

---
+++ header.h
@@ -0,0 +1,10 @@
+#ifndef CALCULATOR_HEADER_H
+#define CALCULATOR_HEADER_H
+
+// Function declarations for basic calculator operations
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+
+#endif // CALCULATOR_HEADER_H
